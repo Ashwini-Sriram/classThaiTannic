2ffd05fcbff351daf36d5a2f81f74aee68132e03
Ashwini-Sriram <58700518+Ashwini-Sriram@users.noreply.github.com>
2021-01-28 11:04:15 +0530

    Create billings.cpp

---
+++ billings.cpp
@@ -0,0 +1,14 @@
+
+#include "billing.h"
+
+
+void display_bill_intro()
+{
+cout<<"\n-------------------------------------------------------"<<endl;
+cout<<"\n                   T.H.A.I T.A.N.N.I.C"<<endl;
+cout<<"\n-------------------------------------------------------"<<endl;
+cout<<"\n                      Restaurant"<<endl;
+cout<<"\n                   Address"<<endl;
+cout<<"\n                   phone"<<endl;
+
+}
